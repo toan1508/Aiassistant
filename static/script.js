88883a64fd358537fe3ebce7911ee51d167dda15
toan1508 <toanhp1508@gmail.com>
2025-06-22 11:37:35 +0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -26,7 +26,7 @@ function appendMessage(role, text) {
 
 async function callGemini(prompt) {
   try {
-    const res = await fetch("http://127.0.0.1:5000/chat", {
+    const res = await fetch("https://aiassistant-5rff.onrender.com/ask", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ prompt })
